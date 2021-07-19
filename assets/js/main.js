d92eb560e9cec4dc2420b4aeddd8eb4d4b294d2e
Tulio-Aguiar <axeaguiar@hotmail.com>
2021-07-19 19:23:40 +0200

    InnerHTML01

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("info").innerHTML += '<h1>Hello World <br></h1>'
+document.getElementById("info").innerHTML += '<h2>How are you?</h2>'
+document.getElementById("container").innerHTML += '<p> Start of the element</p>'
+document.write('end of the element')
